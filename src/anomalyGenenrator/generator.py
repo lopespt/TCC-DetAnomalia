7995b91649f219a36f1ceabd02e06097690eacdf
Kaike <kzuanetti53@gmail.com>
2022-08-18 09:28:13 -0300

    gerador de anomalia

---
+++ src/anomalyGenenrator/generator.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+#Preciso gerar anomalias na base e indentificalas
+#Elas precisam estar numa escala definida no tcc seguindo 2 parametros (intensidade, repetição da base)
+#essas 2 variaveis vão de 20 a 80 %
+#é necessario ter 4 variaveis a base de treino x e y onde x é a base bruta e y é o valor se é anomalia ou não
+# e a base de teste x e y para comparar e fazer as metricas
+
+
+
+
+def anomaly():
+    pass
