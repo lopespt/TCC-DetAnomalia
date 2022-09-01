01a5fd326e5ea89a743fe2e2a246009ee6c1ac88
Kaike <kzuanetti53@gmail.com>
2022-09-01 10:33:28 -0300

    add random forest

--- src/classifiers/tree.py
+++ src/classifiers/tree.py
@@ -1,4 +1,4 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, RandomForestClassifier
 
 
 
@@ -9,7 +9,15 @@ def arvore(train_dataset_x,test_dataset_x,train_dataset_y):
     model = arvRisco.fit(train_dataset_x,train_dataset_y)
 
     return model
-    
+
+
+def aleArvore(train_dataset_x,train_dataset_y,test_dataset_x):
+
+
+    randomForest = RandomForestClassifier(n_estimators=100, criterion="entropy", random_state=0)
+    randomForest.fit(train_dataset_x, train_dataset_y)
+
+    return randomForest.predict(test_dataset_x)
 
 
 
